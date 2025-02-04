8ae8b06c9a3f32df9091a79db478de4a1156379e
bastoss1 <bastien.guidone13@gmail.com>
2025-02-04 14:29:14 +0100

    added dockerfile and configure server

--- my-first-app/WeatherForecast.cs
+++ my-first-app/WeatherForecast.cs
@@ -5,10 +5,8 @@ namespace myfirstapp
     public class WeatherForecast
     {
         public DateTime Date { get; set; }
-
         public int TemperatureC { get; set; }
-
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string Summary { get; set; }
+        public string Summary { get; set; } = string.Empty;
     }
+}
--- my-first-app/obj/Release/net9.0/my-first-app.AssemblyInfo.cs
+++ my-first-app/obj/Release/net9.0/my-first-app.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("my-first-app")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9056235b42d9f27cc08510b2fc939053dac5f704")]
 [assembly: System.Reflection.AssemblyProductAttribute("my-first-app")]
